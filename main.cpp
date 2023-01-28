48919677a2cf33d4c69b3b6477f31e0f1d1f02ed
Andrei Florea <andreifl@duck.com>
2023-01-28 17:28:17 +0200

    Create window

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <graphics.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    constexpr short WINDOW_WIDTH = 800;
+    constexpr short WINDOW_HEIGHT = 600;
+
+    initwindow(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+
+
+    getch();
+    closegraph();
+
     return 0;
 }
