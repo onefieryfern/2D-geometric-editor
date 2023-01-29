a4b4223c106029895828c59e9a2b7db94d628967
Andrei Florea <andreifl@duck.com>
2023-01-29 09:42:00 +0000

    Add title to window

--- main.cpp
+++ main.cpp
@@ -8,12 +8,14 @@ int main() {
     constexpr short WINDOW_WIDTH = 800;
     constexpr short WINDOW_HEIGHT = 600;
 
+    constexpr char WINDOW_TITLE[] = "2D Geometric Editor";
+
     const Point SCREEN_CENTRE { WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 };
 
     const short POINT_SIZE = 4;
 
 
-    initwindow(WINDOW_WIDTH, WINDOW_HEIGHT);
+    initwindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 
 
     drawPoint(SCREEN_CENTRE, POINT_SIZE);
