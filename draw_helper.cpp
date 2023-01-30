aaefaaa42ac5f7eec6da961effb4cfd8e981bd31
Andrei Florea <andreifl@duck.com>
2023-01-30 20:06:16 +0000

    Fix style

--- draw_helper.cpp
+++ draw_helper.cpp
@@ -7,6 +7,6 @@ void drawPoint (const Point& point, short pointSize) {
     fillellipse(point.x, point.y, pointSize, pointSize);
 }
 
-void drawLine(const Point &endpoint1, const Point &endpoint2) {
+void drawLine (const Point& endpoint1, const Point& endpoint2) {
     line(endpoint1.x, endpoint1.y, endpoint2.x, endpoint2.y);
 }
--- geometry_data_structures.cpp
+++ geometry_data_structures.cpp
@@ -9,5 +9,5 @@ PointInteractionBox createPointInteractionBox (const Point& point, short pointSi
     pointInteractionBox.bottomRight.x = static_cast<short>(point.x + pointSize);
     pointInteractionBox.bottomRight.y = static_cast<short>(point.y + pointSize);
 
-    return  pointInteractionBox;
+    return pointInteractionBox;
 }
